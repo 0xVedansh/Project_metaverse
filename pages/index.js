9b436ef17b1e4f3ce72e93d4a3fb9049acc1429a
0xVedansh <vedansh83138@gmail.com>
2023-06-19 01:30:49 +0530

    ready for deployment

--- pages/index.js
+++ pages/index.js
@@ -11,15 +11,15 @@ const Page = () => (
       <Explore />
     </div>
     <div className="relative">
-        <GetStarted />
-        <div className="gradient-04 z-0" />
-        <WhatsNew />
+      <GetStarted />
+      <div className="gradient-04 z-0" />
+      <WhatsNew />
     </div>
     <World />
     <div className="relative">
-        <Insights />
-        <div className="gradient-04 z-0" />
-        <Feedback />
+      <Insights />
+      <div className="gradient-04 z-0" />
+      <Feedback />
     </div>
     <Footer />
   </div>
